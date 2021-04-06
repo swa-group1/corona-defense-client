60328f9992705e2da1bb7308d7b977ed55197bcb
hermannm <hermann.morkrid@gmail.com>
2021-04-06 14:59:04 +0200

    convert default files to Kotlin

---
+++ desktop/src/com/coronadefense/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.coronadefense.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.coronadefense.Game
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(Game(), config)
+    }
+}
