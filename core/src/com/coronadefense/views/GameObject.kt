3daeded8fa72b060786b8aebaf7a779f202af295
hermannm <hermann.morkrid@gmail.com>
2021-04-07 13:11:35 +0200

    add MovingGameObject logic for changing target position

--- core/src/com/coronadefense/views/GameObject.kt
+++ core/src/com/coronadefense/views/GameObject.kt
@@ -2,15 +2,15 @@ package com.coronadefense.views
 
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
 
 open class GameObject(
   texturePath: String,
-  var x: Float,
-  var y: Float
+  var position: Vector2
 ) {
   private val texture: Texture = Texture(texturePath)
   fun draw(sprites: SpriteBatch) {
-    sprites.draw(texture, x, y)
+    sprites.draw(texture, position.x, position.y)
   }
   fun dispose() {
     texture.dispose()
--- core/src/com/coronadefense/views/MovingGameObject.kt
+++ core/src/com/coronadefense/views/MovingGameObject.kt
@@ -1,29 +1,42 @@
 package com.coronadefense.views
 
-import com.badlogic.gdx.math.Vector3
+import com.badlogic.gdx.math.Vector2
+import java.util.Stack
 import kotlin.math.atan
 import kotlin.math.cos
 import kotlin.math.sin
 
 class MovingGameObject(
   texturePath: String,
-  x: Float,
-  y: Float,
-  targetPositions: Array<Pair<Float, Float>>,
-  val endX: Float,
-  val endY: Float,
-  val speed: Float
-) : GameObject(texturePath, x, y) {
-  private val angle = atan((endY-y)/(endX-x))
-  private val velocity: Vector3 = Vector3(
-    speed * cos(angle),
-    speed * sin(angle),
-    0f
-  )
+  position: Vector2,
+  private val targetPositions: Stack<Vector2>,
+  private val speed: Float
+) : GameObject(texturePath, position) {
+  fun calculateVelocity(): Vector2 {
+    val angle = atan(
+      (targetPositions.peek().x - position.x) / (targetPositions.peek().y - position.x)
+    )
+    return Vector2(
+      speed * cos(angle),
+      speed * sin(angle)
+    )
+  }
+  private var velocity: Vector2 = calculateVelocity()
   fun update(deltaTime: Float) {
     velocity.scl(deltaTime)
-    this.x += velocity.x
-    this.y += velocity.y
+    val nextPosition = targetPositions.peek()
+    if (
+      (velocity.x >= 0 && this.position.x + velocity.x > nextPosition.x)
+      || (velocity.x < 0 && this.position.x + velocity.x < nextPosition.x)
+      || (velocity.y >= 0 && this.position.y + velocity.y > nextPosition.y)
+      || (velocity.y < 0 && this.position.y + velocity.y < nextPosition.y)
+    ) {
+      targetPositions.pop()
+      velocity = calculateVelocity()
+      velocity.scl(deltaTime)
+    }
+    this.position.x += velocity.x
+    this.position.y += velocity.y
     velocity.scl(1/deltaTime)
   }
 }
