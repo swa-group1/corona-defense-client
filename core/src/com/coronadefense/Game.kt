a06c460eabfee386f2797ee1d8f780392fb0b27d
hermannm <hermann.morkrid@gmail.com>
2021-04-14 15:46:25 +0200

    implement getHighScoreList in ApiClient

--- core/src/com/coronadefense/Game.kt
+++ core/src/com/coronadefense/Game.kt
@@ -4,21 +4,35 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.coronadefense.api.ApiClient
+import com.coronadefense.api.HighScore
+import kotlinx.coroutines.*
 
 class Game : ApplicationAdapter() {
     var batch: SpriteBatch? = null
     var img: Texture? = null
+    val apiClient: ApiClient = ApiClient()
+    var highScores: List<HighScore>? = null
+    val font: BitmapFont = BitmapFont()
     override fun create() {
         batch = SpriteBatch()
         img = Texture("badlogic.jpg")
+        GlobalScope.launch {
+            highScores = apiClient.getHighScoreList()
+        }
     }
 
     override fun render() {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         batch?.begin()
-        batch?.draw(img, 0f, 0f)
+        if (highScores != null) {
+            for (i in 0..highScores!!.size) {
+                font.draw(batch, "${highScores!![i].name}: ${highScores!![i].value}", 10f, 10f * (i + 1))
+            }
+        }
         batch?.end()
     }
 
--- core/src/com/coronadefense/api/ApiClient.kt
+++ core/src/com/coronadefense/api/ApiClient.kt
@@ -1,4 +1,31 @@
 package com.coronadefense.api
 
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.serialization.*
+
+const val baseUrl = "localhost:5000"
+
+@Serializable
+data class HighScore(val name: String, val value: Int)
+
+@Serializable
+data class HighScoreList(
+  val scores: List<HighScore>,
+  val details: String,
+  val success: Boolean
+)
+
 class ApiClient {
+  val client = HttpClient(CIO)
+  suspend fun getHighScoreList(): List<HighScore> {
+    val highScoreList = client.get<HighScoreList>("${baseUrl}/HighScoreList")
+    if (highScoreList.success) {
+      return highScoreList.scores
+    } else {
+      throw Exception("Failed to fetch high score list.")
+    }
+  }
 }
