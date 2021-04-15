c6b52fac1434672b2811dc28cb2e9ed75fc1a0ea
Jorunn <jorunn@selbu.org>
2021-04-15 15:30:02 +0200

    bug fixes in Game after merge

--- core/src/com/coronadefense/Game.kt
+++ core/src/com/coronadefense/Game.kt
@@ -4,7 +4,6 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.InputMultiplexer
 import com.badlogic.gdx.graphics.GL20
-<<<<<<< HEAD
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
@@ -13,6 +12,12 @@ import com.coronadefense.api.HighScore
 import kotlinx.coroutines.*
 
 class Game : ApplicationAdapter() {
+    companion object {
+        const val WIDTH = 800F
+        const val HEIGHT = 480F
+        const val TITLE = "Game"
+        var batch: SpriteBatch? = null
+    }
     var batch: SpriteBatch? = null
     var img: Texture? = null
     val apiClient: ApiClient = ApiClient()
@@ -24,29 +29,9 @@ class Game : ApplicationAdapter() {
         GlobalScope.launch {
             highScores = apiClient.getHighScoreList()
         }
-=======
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.coronadefense.states.MenuState
-
-class Game : ApplicationAdapter() {
-    companion object {
-        const val WIDTH = 800F
-        const val HEIGHT = 480F
-        const val TITLE = "Game"
-        var batch: SpriteBatch? = null
-    }
-    private var stateManager: GameStateManager? = null
-    override fun create() {
-        batch = SpriteBatch()
-        stateManager = GameStateManager()
-        Gdx.input.inputProcessor = InputMultiplexer()
-        Gdx.gl.glClearColor(0F, 0F, 0F, 1F)
-        stateManager?.push(MenuState(stateManager!!))
->>>>>>> main
     }
     override fun render() {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-<<<<<<< HEAD
         batch?.begin()
         if (highScores != null) {
             for (i in highScores!!.indices) {
@@ -56,10 +41,6 @@ class Game : ApplicationAdapter() {
             }
         }
         batch?.end()
-=======
-        stateManager?.update(Gdx.graphics.deltaTime)
-        stateManager?.render(batch!!)
->>>>>>> main
     }
 
     override fun dispose() {
