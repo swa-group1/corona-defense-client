22b6c0027156094e247ece02726b1483e38a9e28
le0-0 <leonard.schoyen@gmail.com>
2021-04-18 16:47:49 +0200

    BugFix: Receiver did not recognize new MessageTypes from Socket format 0.1.

--- core/src/com/coronadefense/receiver/Receiver.kt
+++ core/src/com/coronadefense/receiver/Receiver.kt
@@ -82,22 +82,11 @@ class Receiver(private val observers: List<IReceiverObserver>) {
 
     @ExperimentalUnsignedTypes
     private fun getMessageType(byteCode: Byte): MessageType {
-        when (byteCode) {
-            MessageType.PING.byteCode -> return MessageType.PING
-            MessageType.FIGHT_ROUND.byteCode -> return MessageType.FIGHT_ROUND
-            MessageType.GAME_MODE.byteCode -> return MessageType.GAME_MODE
-            MessageType.INPUT_ROUND.byteCode -> return MessageType.INPUT_ROUND
-            MessageType.LOBBY_MODE.byteCode -> return MessageType.LOBBY_MODE
-            MessageType.HEALTH_UPDATE.byteCode -> return MessageType.HEALTH_UPDATE
-            MessageType.MONEY_UPDATE.byteCode -> return MessageType.MONEY_UPDATE
-            MessageType.PLAYER_COUNT_UPDATE.byteCode -> return MessageType.PLAYER_COUNT_UPDATE
-            MessageType.TOWER_POSITION.byteCode -> return MessageType.TOWER_POSITION
-            MessageType.TOWER_REMOVED.byteCode -> return MessageType.TOWER_REMOVED
-            MessageType.ANIMATION_CONFIRMATION.byteCode -> return MessageType.ANIMATION_CONFIRMATION
-            MessageType.BOARD_TO_PATH_ANIMATION.byteCode -> return MessageType.BOARD_TO_PATH_ANIMATION
-            MessageType.PATH_TO_PATH_ANIMATION.byteCode -> return MessageType.PATH_TO_PATH_ANIMATION
-            MessageType.TOWER_ANIMATION.byteCode -> return MessageType.TOWER_ANIMATION
-            else -> throw IllegalArgumentException("Byte code $byteCode not valid")
+        for (messageType: MessageType in MessageType.values()) {
+            if (messageType.byteCode == byteCode) {
+                return messageType
+            }
         }
+        throw IllegalArgumentException("Byte code $byteCode not valid")
     }
 }
