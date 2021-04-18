a85880e8175a0e015813ccab8c980212eb04cec3
hermannm <hermann.morkrid@gmail.com>
2021-04-18 11:11:19 +0200

    add Lobby class

---
+++ core/src/com/coronadefense/api/Lobby.kt
@@ -0,0 +1,22 @@
+package com.coronadefense.api
+
+import kotlinx.coroutines.*
+
+class Lobby(
+  val id: Long,
+  val name: String,
+  val accessToken: Long,
+) {
+  var playerCount: Int = 1
+    set(value) { if (value > 0) field = value }
+  constructor(
+    id: Long, name: String, accessToken: Long, playerCount: Int
+  ) : this(id, name, accessToken) {
+    this.playerCount = playerCount
+  }
+  fun leaveLobby() {
+    GlobalScope.launch {
+      ApiClient.leaveLobbyRequest(id, accessToken)
+    }
+  }
+}
