3a3785081981a3013e8261149e708b3a514a2f9c
hermannm <hermann.morkrid@gmail.com>
2021-04-18 10:34:07 +0200

    make ApiClient singleton

--- core/src/com/coronadefense/Game.kt
+++ core/src/com/coronadefense/Game.kt
@@ -12,45 +12,41 @@ import com.coronadefense.api.HighScore
 import kotlinx.coroutines.*
 
 class Game : ApplicationAdapter() {
-    companion object {
-        const val WIDTH = 800F
-        const val HEIGHT = 480F
-        const val TITLE = "Game"
-        var batch: SpriteBatch? = null
-    }
+  companion object {
+    const val WIDTH = 800F
+    const val HEIGHT = 480F
+    const val TITLE = "Game"
     var batch: SpriteBatch? = null
-    var img: Texture? = null
-    val apiClient: ApiClient = ApiClient()
-    var highScores: List<HighScore>? = null
-    var lobbyId: Long? = null
-    var font: BitmapFont? = null
-    override fun create() {
-        batch = SpriteBatch()
-        font = BitmapFont()
-        GlobalScope.launch {
-            lobbyId = apiClient.createLobbyRequest("Hermanns", "test")
-            highScores = apiClient.highScoreListRequest()
-        }
+  }
+  var batch: SpriteBatch? = null
+  var img: Texture? = null
+  var highScores: List<HighScore>? = null
+  var lobbyId: Long? = null
+  var font: BitmapFont? = null
+  override fun create() {
+    batch = SpriteBatch()
+    font = BitmapFont()
+    GlobalScope.launch {
+      lobbyId = ApiClient.createLobbyRequest("Hermanns", "test")
+      highScores = ApiClient.highScoreListRequest()
     }
-    override fun render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch?.begin()
-        if (lobbyId != null) {
-            font!!.draw(
-              batch, "$lobbyId", 10f, 30f
-            )
-        }
-        if (highScores != null) {
-            for (i in highScores!!.indices) {
-                font!!.draw(
-                  batch, "${highScores!![i].name}: ${highScores!![i].value}", 10f, 60f + 30f * (i)
-                )
-            }
-        }
-        batch?.end()
+  }
+  override fun render() {
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+    batch?.begin()
+    if (lobbyId != null) {
+      font!!.draw(batch, "$lobbyId", 10f, 30f)
     }
-
-    override fun dispose() {
-        batch?.dispose()
+    if (highScores != null) {
+      for (i in highScores!!.indices) {
+        font!!.draw(
+          batch, "${highScores!![i].name}: ${highScores!![i].value}", 10f, 60f + 30f * (i)
+        )
+      }
     }
+    batch?.end()
+  }
+  override fun dispose() {
+    batch?.dispose()
+  }
 }
