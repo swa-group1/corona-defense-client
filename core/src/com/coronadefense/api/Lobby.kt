934c68e4c524a259d2da093f86bec68a721e9a2b
hermannm <hermann.morkrid@gmail.com>
2021-04-20 23:35:30 +0200

    remove redundant constructor

--- core/src/com/coronadefense/api/Lobby.kt
+++ core/src/com/coronadefense/api/Lobby.kt
@@ -7,14 +7,8 @@ class Lobby(
         val id: Long,
         val name: String,
         val accessToken: Long,
+        val playerCount: Int,
 ) {
-  var playerCount: Int = 1
-    set(value) { if (value >= 0) field = value }
-  constructor(
-          id: Long, name: String, accessToken: Long, playerCount: Int
-  ) : this(id, name, accessToken) {
-    this.playerCount = playerCount
-  }
   fun leaveLobby() {
     GlobalScope.launch {
       ApiClient.leaveLobbyRequest(id, accessToken)
