c0ada13629c52753f06cd92f2d022c7e9ea48d53
hermannm <hermann.morkrid@gmail.com>
2021-04-22 15:09:05 +0200

    switch Textures maps to functions for modifiability

--- core/src/com/coronadefense/states/playStates/Textures.kt
+++ core/src/com/coronadefense/states/playStates/Textures.kt
@@ -1,32 +1,16 @@
 package com.coronadefense.states.playStates
 
 object Textures {
-  val towers: Map<UByte, String> = hashMapOf(
-    100.toUByte() to "towers/100.png",
-    102.toUByte() to "towers/102.png",
-    104.toUByte() to "towers/104.png",
-          106.toUByte() to "towers/106.png"
-  )
-  val stages: Map<Int, String> = hashMapOf(
-    0 to "stages/samfundetStage.png"
-  )
-  val intruders: Map<UByte, String> = hashMapOf(
-          0.toUByte() to "intruders/000.png",
-          5.toUByte() to "intruders/005.png",
-          6.toUByte() to "intruders/006.png",
-          10.toUByte() to "intruders/010.png",
-          11.toUByte() to "intruders/011.png",
-          12.toUByte() to "intruders/012.png",
-          13.toUByte() to "intruders/013.png",
-          14.toUByte() to "intruders/014.png",
-          15.toUByte() to "intruders/015.png",
-          16.toUByte() to "intruders/016.png",
-          17.toUByte() to "intruders/017.png",
-  )
-  val projectiles: Map<UByte, String> = hashMapOf(
-          101.toUByte() to "projectiles/101.png",
-          103.toUByte() to "projectiles/103.png",
-          105.toUByte() to "projectiles/105.png",
-          107.toUByte() to "projectiles/107.png"
-  )
+  fun stagePath(stageNumber: Int): String {
+    return "stages/stage_${stageNumber.toString().padStart(3, '0')}_img.png"
+  }
+  fun intruderPath(spriteNumber: UByte): String {
+    return "intruders/${spriteNumber.toString().padStart(3, '0')}.png"
+  }
+  fun towerPath(typeNumber: UByte): String {
+    return "towers/1${((typeNumber.toInt() - 1) * 2).toString().padStart(2, '0')}.png"
+  }
+  fun projectilePath(towerTypeNumber: UByte): String {
+    return "projectiles/1${((towerTypeNumber.toInt() * 2) - 1).toString().padStart(2, '0')}.png"
+  }
 }
