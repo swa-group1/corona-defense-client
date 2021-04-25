1d6506084d3127ce8e840ab4c4c2e0f80b3b186a
hermannm <hermann.morkrid@gmail.com>
2021-04-25 12:52:43 +0200

    add tutorial state and menu button

---
+++ core/src/com/coronadefense/states/menuStates/TutorialState.kt
@@ -0,0 +1,45 @@
+package com.coronadefense.states.menuStates
+
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.coronadefense.states.StateManager
+import com.coronadefense.states.InputState
+import com.coronadefense.utils.BackButton
+import com.coronadefense.utils.Constants.GAME_HEIGHT
+import com.coronadefense.utils.Constants.GAME_WIDTH
+import com.coronadefense.utils.Textures
+
+/**
+ * State to show a tutorial of how to play Corona Defense.
+ * @param stateManager Manager of all game states.
+ */
+class TutorialState(
+  stateManager: StateManager
+): InputState(stateManager){
+  private val background: Texture = Texture(Textures.background("tutorial"))
+
+  private val backButton = BackButton("MainMenu", stateManager, stage)
+
+  override fun update(deltaTime: Float) {
+    backButton.update()
+  }
+
+  override fun render(sprites: SpriteBatch) {
+    sprites.projectionMatrix = camera.combined
+    sprites.begin()
+
+    sprites.draw(background, 0f, 0f, GAME_WIDTH, GAME_HEIGHT)
+
+    sprites.end()
+    super.draw()
+  }
+
+  override fun dispose() {
+    super.dispose()
+
+    background.dispose()
+    backButton.dispose()
+
+    println("TutorialState disposed")
+  }
+}
